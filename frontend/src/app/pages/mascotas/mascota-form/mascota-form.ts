c61dad21d9e35d8b58880a4d6ddde086e96f16a4
Bryann12S <bryanshiguango6@gmail.com>
2026-05-05 17:50:27 -0500

    add mascotas form and list

---
+++ frontend/src/app/pages/mascotas/mascota-form/mascota-form.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mascota-form',
+  imports: [],
+  templateUrl: './mascota-form.html',
+  styleUrl: './mascota-form.css',
+})
+export class MascotaForm {}
---
+++ frontend/src/app/pages/mascotas/mascota-list/mascota-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mascota-list',
+  imports: [],
+  templateUrl: './mascota-list.html',
+  styleUrl: './mascota-list.css',
+})
+export class MascotaList {}
