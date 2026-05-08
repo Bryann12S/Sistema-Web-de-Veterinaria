e54f4c69b2fdc59360444b3a13d2de298ebd0e96
Bryann12S <bryanshiguango6@gmail.com>
2026-05-08 00:40:53 -0500

    Cambio de carpeta de cita a pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,8 +7,8 @@ import { Login } from './pages/login/login';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { MascotaList } from './pages/mascotas/mascota-list/mascota-list';
 import { MascotaForm } from './pages/mascotas/mascota-form/mascota-form';
-import { CitaForm } from './components/cita/cita-form/cita-form';
-import { CitaList } from './components/cita/cita-list/cita-list';
+import { CitaForm } from './pages/cita/cita-form/cita-form';
+import { CitaList } from './pages/cita/cita-list/cita-list';
 
 
 export const routes: Routes = [
